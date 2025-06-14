b95588153446e3c68dddcb73d478fc7715c90af3
Khor Kean Teng <u2004763@siswa.um.edu.my>
2025-06-14 17:19:17 +0800

    auto commit #61

---
+++ bin/to-pdf.sh
@@ -0,0 +1,4 @@
+export PATH="$PATH:/C/Users/Khor Kean Teng/AppData/Local/Programs/Python/Python312/Scripts"
+export PANDOC_PATH="/C/Users/Khor Kean Teng/Downloads/pandoc-3.7.0.2-windows-x86_64/pandoc-3.7.0.2"
+export PATH="$PATH:$PANDOC_PATH"
+jupyter nbconvert --to html evaluation/explainability.ipynb
